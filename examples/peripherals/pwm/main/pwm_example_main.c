4a3e50f18995af04cd93ee732381e0a95bdcaf48
wangsong <13013086711@163.com>
2023-11-02 20:52:54 +0800

    基于硬件定时器实现呼吸灯

--- examples/peripherals/pwm/main/pwm_example_main.c
+++ examples/peripherals/pwm/main/pwm_example_main.c
@@ -1,11 +1,5 @@
-/* pwm example
+/* PWM呼吸灯示例工程 */
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 
 #include <stdio.h>
 #include <string.h>
@@ -15,114 +9,111 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
+#include "driver/pwm.h"                 //pwm头文件
+#include "driver/gpio.h"
+
 #include "esp_log.h"
 #include "esp_system.h"
-#include "esp_err.h"
-
-#include "esp8266/gpio_register.h"
-#include "esp8266/pin_mux_register.h"
-
-#include "driver/pwm.h"
-
+#include "driver/uart.h"
 #include "driver/hw_timer.h"
-#include "rom/ets_sys.h"
+
+#define PWM_PERIOD 1000                 //PWM周期（1000us）
+#define CHANNLE_PWM_TOTAL 1             //通道总数
+#define CHANNLE_PWM_LED1 0              //PWM通道号
+#define CHANNLE_PWM_IO_NUM_LED1 2      //PWM输出引脚号(LED1)
+
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL << CHANNLE_PWM_IO_NUM_LED1) | (1ULL << CHANNLE_PWM_IO_NUM_LED1))
 
 
+static uint32_t pinNum[CHANNLE_PWM_TOTAL] = {CHANNLE_PWM_IO_NUM_LED1};//PWM通道总数
+static uint32_t setDuties[CHANNLE_PWM_TOTAL] = {50};                  //PWM通道周期（1000us）
+static int16_t phase[CHANNLE_PWM_TOTAL] = {0};                        //相位
 
+static uint8_t ledDutyFlag = 1;         //呼吸灯标志位
+static uint8_t ledDutySteup = 5;        //呼吸灯占空比变化步长
+static uint32_t duty = 1000;            //呼吸灯占空比变量
+#define TEST_ONE_SHOT    false        // testing will be done without auto reload (one-shot)
+#define TEST_RELOAD      true         // testing will be done with auto reload
+os_timer_t os_timer;                    //定义定时器结构体
+static const char *TAG = "main";		//格式化输出前缀
+static volatile int state = 0;
 
-#define PWM_0_OUT_IO_NUM   2
-#define PWM_1_OUT_IO_NUM   13
-#define PWM_2_OUT_IO_NUM   14
-#define PWM_3_OUT_IO_NUM   15
-
-// PWM period 1000us(1Khz), same as depth
-#define PWM_PERIOD    (1000)
-
-static const char *TAG = "pwm_example";
-os_timer_t os_timer_1;
-
-// pwm pin number
-const uint32_t pin_num[4] = {
-    PWM_0_OUT_IO_NUM,
-    PWM_1_OUT_IO_NUM,
-    PWM_2_OUT_IO_NUM,
-    PWM_3_OUT_IO_NUM
-};
-
-// duties table, real_duty = duties[x]/PERIOD
-uint32_t duties[4] = {
-    0, 1000, 1000, 1000,
-};
-
-// phase table, delay = (phase[x]/360)*PERIOD
-float phase[4] = {
-    0, 0, 90.0, -90.0,
-};
-
-void OS_Timer_1_Cb(void);//定时器回调函数
-void OS_Timer_Init(uint32_t ms,bool repeat_flag);//定时器初始化函数
-
-
-void OS_Timer_1_Cb(void)
+static void pwm_breathe_led(void *arg)
 {
-	static uint8_t direction=0;
-	int span = 10;//每次的增量或减量
 
-	if(direction==0 && duties[0]<=1000)//占空比增大
-	   {
-		    duties[0]+=span;
-	    }
-	if(direction==1 && duties[0]>=0)//占空比减小
-		{
-			duties[0]-=span;
-		}
-	if(duties[0]>1000)//改变方向
-	    {
-		  direction=1;
-	    }
-	if(duties[0]<=span)//改变方向
-		{
-		  direction=0;
-		}
-	pwm_set_duty(duties[0],0);//设置占空比
-	pwm_start();
+    if(ledDutyFlag == 1)
+    {
+        duty = duty + ledDutySteup;
+        if(duty >= PWM_PERIOD)
+            ledDutyFlag = 0;
+    }
+    else
+    {
+        duty = duty - ledDutySteup;
+        if(duty <= 0)
+            ledDutyFlag = 1;
+    }
+    
+    pwm_set_duty(CHANNLE_PWM_LED1, duty);//设置周期
+    pwm_start();                         //PWM启动    
 }
 
-
-//软件定时器配置初始化
-void OS_Timer_Init(uint32_t ms,bool repeat_flag)
+void hw_timer_callback1(void *arg)
 {
-	os_timer_disarm(&os_timer_1);//关闭软件定时器
-	os_timer_setfn(&os_timer_1,(os_timer_func_t*)OS_Timer_1_Cb,NULL);//注册软件定时器回调函数
-	os_timer_arm(&os_timer_1,ms,repeat_flag);//打开软件定时器，设置定时周期，设置是否自动重装
-}
 
-
-void app_main()
-{
-    pwm_init(PWM_PERIOD, duties, 4, pin_num);
-    pwm_set_phases(phase);
-    // pwm_start();
-    int16_t count = 0;
-
-
-    OS_Timer_Init(10,1);//10ms改变一次占空比,重装载
-
-
-    // while (1) {
-    //     if (count == 20) {
-    //         // channel0, 1 output hight level.
-    //         // channel2, 3 output low level.
-    //         pwm_stop(0x3);
-    //         ESP_LOGI(TAG, "PWM stop\n");
-    //     } else if (count == 30) {
-    //         pwm_start();
-    //         ESP_LOGI(TAG, "PWM re-start\n");
-    //         count = 0;
-    //     }
-
-    //     count++;
-    //     vTaskDelay(1000 / portTICK_RATE_MS);
+    state++;
+    // if(state == 1000000){
+    //     gpio_set_level(CHANNLE_PWM_IO_NUM_LED1, 0);
     // }
+
+    // printf("sss");
+    // ESP_LOGI(TAG, "pirnt x %d",state);
 }
+
+void app_main(void)
+{
+    uart_set_baudrate(UART_NUM_0,115200);//初始化波特率为115200
+
+    ESP_LOGI(TAG, "Config gpio");
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+
+    gpio_set_level(CHANNLE_PWM_IO_NUM_LED1, 1);
+    ESP_LOGI(TAG, "LED1 blink\n");
+	pwm_init(PWM_PERIOD, setDuties, CHANNLE_PWM_TOTAL, pinNum);     //PWM初始化
+    pwm_set_phases(phase);               //设置相位
+    // pwm_set_duty(CHANNLE_PWM_LED1, duty);//设置周期
+	// pwm_start();                         //PWM启动  
+	// ESP_LOGI(TAG, "started!!!");
+    // os_timer_disarm(&os_timer);          //关闭定时器
+	// ESP_LOGI(TAG, "disarm");
+    // os_timer_setfn(&os_timer, (os_timer_func_t *)(pwm_breathe_led), NULL);//初始化定时器的回调函数(呼吸灯)
+    // os_timer_arm(&os_timer, 100000, 0);      //启动定时器
+	// ESP_LOGI(TAG, "start timerarm");
+
+
+    ESP_LOGI(TAG, "Initialize hw_timer for callback1");
+    hw_timer_init(pwm_breathe_led, NULL);
+    ESP_LOGI(TAG, "Set hw_timer timing time 100us with reload");
+    hw_timer_alarm_us(10000, TEST_RELOAD);
+    vTaskDelay(1000 / portTICK_RATE_MS);
+    ESP_LOGI(TAG, "Deinitialize hw_timer for callback1");
+    // hw_timer_deinit();
+
+
+
+
+    for(;;)                              
+    {
+        vTaskDelay(1000 / portTICK_RATE_MS);                       
+        ESP_LOGI(TAG,"for:%d",duty);
+        // hw_timer_callback1(NULL);
+    }
+}
+
 
